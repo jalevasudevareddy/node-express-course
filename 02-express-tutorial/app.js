7f4981f14e669c2bdff6bc3d539165432e4ace05
VasudevaReddy Jale <103563671+jalevasudevareddy@users.noreply.github.com>
2024-09-22 10:54:41 +0530

    After complition of course
    After complition of course

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,14 @@
-console.log('Express Tutorial')
+console.log("Express Tutorial");
+
+const express = require("express");
+const app = express();
+const path = require("path");
+app.use(express.static("./public"));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./navbar-app/index.html"));
+// });
+
+app.listen(5001, () => {
+  console.log("Express server listening on:5001");
+});
